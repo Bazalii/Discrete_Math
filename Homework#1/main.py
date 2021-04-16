2cb72004d215723f2f7343250d5f7edf2b06b0e0
Bazalii Ivan <ivan@localhost.localdomain>
2021-04-16 22:49:40 +0300

    New pictures+fixes

--- Homework#1/main.py
+++ Homework#1/main.py
@@ -6,7 +6,7 @@ from networkx.algorithms import planar_drawing
 import matplotlib.pyplot as plt
 import graphviz as gv
 import dwave_networkx as dnx
-colors = ['green', 'red', 'orange', 'yellow', 'pink', 'blue', 'purple', 'black']
+colors = ['green', 'red', 'orange', 'yellow', 'pink', 'blue', 'purple', 'olive', 'magenta']
 
 def color_the_edges(inp_graph, type_of_coloring):
     buffer = []
@@ -33,6 +33,7 @@ def color_the_vertexes(inp_graph, type_of_coloring):
 
 countries = []
 edges = []
+nw_edges = []
 fout = open('list_of_enumerate_vertexes.txt', 'w')
 with open('input.txt') as file:
     for line in file:
@@ -44,18 +45,23 @@ with open('input.txt') as file:
             countries.append(input_data[1])
         if input_data[1] == 'VA':
             edges.append((input_data[0], input_data[1], int(input_data[2])))
+            nw_edges.append((input_data[0], input_data[1]))
         if int(input_data[2]) != 0:
             edges.append((input_data[0], input_data[1], int(input_data[2])))
+            nw_edges.append((input_data[0], input_data[1]))
 print(len(countries))
 Full_graph = nx.Graph()
+nw_Full = nx.Graph()
 Full_graph.add_nodes_from(countries)
+nw_Full.add_nodes_from(countries)
 print(edges)
 Full_graph.add_weighted_edges_from(edges, weight="weight")
+nw_Full.add_edges_from(nw_edges)
 print(Full_graph.edges)
-print(nx.algorithms.check_planarity(Full_graph))
 nx.draw_planar(Full_graph, with_labels=True, font_weight='bold')
-# plt.show()
+plt.show()
 Main_graph = Full_graph.subgraph(max(nx.connected_components(Full_graph)))
+nw_Main = nw_Full.subgraph(max(nx.connected_components(Full_graph)))
 print("b")
 print("Number of nodes:", Full_graph.number_of_nodes())
 print("Number of edges:", Full_graph.number_of_edges())
@@ -115,11 +121,38 @@ print(len(approximation.maximum_independent_set(Main_graph)))
 print("g")
 maximum_matching = nx.max_weight_matching(Main_graph)
 maximum_matching_graph = gv.Graph()
-print("Maximum matching:", nx.max_weight_matching(Main_graph))
-print(len(nx.max_weight_matching(Main_graph)))
+print("Maximum matching:", maximum_matching)
+print(len(maximum_matching))
+print("nwMaximum matching:", nx.max_weight_matching(nw_Main))
+print(len(nx.max_weight_matching(nw_Main)))
+for i in range (20):
+    print(nx.maximal_matching(Main_graph))
+    print(len(nx.maximal_matching(Main_graph)))
+
+print(nx.is_maximal_matching(Main_graph, maximum_matching))
 color_the_edges(maximum_matching_graph, maximum_matching)
 # maximum_matching_graph.view(filename='maximum_matching')
 print("h")
+# for a in Main_graph.nodes:
+#     for b in Main_graph.nodes:
+#         for c in Main_graph.nodes:
+#             for d in Main_graph.nodes:
+#                 for e in Main_graph.nodes:
+#                     for f in Main_graph.nodes:
+#                         for g in Main_graph.nodes:
+#                             for h in Main_graph.nodes:
+#                                 for i in Main_graph.nodes:
+#                                     for j in Main_graph.nodes:
+#                                         for k in Main_graph.nodes:
+#                                             for l in Main_graph.nodes:
+#                                                 for m in Main_graph.nodes:
+#                                                     for n in Main_graph.nodes:
+#                                                         for o in Main_graph.nodes:
+#                                                             for p in Main_graph.nodes:
+#                                                                 vertex_cover = {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p}
+#                                                                 if dnx.is_vertex_cover(Main_graph, vertex_cover):
+#                                                                     print(vertex_cover)
+
 # minimum_vertex_cover = nx.algorithms.minimum_node_cut(Main_graph)
 # minimum_vertex_cover_graph = gv.Graph()
 # print("Minimum vertex cover:", minimum_vertex_cover)
@@ -134,12 +167,53 @@ print(len(minimum_edge_cover))
 color_the_edges(minimum_edge_cover_graph, minimum_edge_cover)
 # minimum_edge_cover_graph.view(filename='minimum_edge_cover')
 print("l")
+block_cut_tree_graph = gv.Graph()
+k = 0
 for i in nx.algorithms.components.biconnected_components(Full_graph):
-    print(i)
+    for j in i:
+        print(j, end=",")
+    print()
+for i in nx.algorithms.components.biconnected_components(Full_graph):
+    for j in i:
+        block_cut_tree_graph.node(j, style='filled', fillcolor=colors[k])
+    k += 1
+for edge in Full_graph.edges:
+    block_cut_tree_graph.edge(edge[0], edge[1])
+# block_cut_tree_graph.view(filename='block_cut_tree_graph')
+block_cut_tree = gv.Graph()
+block_cut_tree.node('2', style='filled', fillcolor=colors[0])
+block_cut_tree.node('3', style='filled', fillcolor=colors[1])
+block_cut_tree.node('8', style='filled', fillcolor=colors[2])
+block_cut_tree.node('7', style='filled', fillcolor=colors[3])
+block_cut_tree.node('4', style='filled', fillcolor=colors[5])
+block_cut_tree.node('6', style='filled', fillcolor=colors[6])
+block_cut_tree.node('1', style='filled', fillcolor=colors[7])
+block_cut_tree.node('5', style='filled', fillcolor=colors[8])
+block_cut_tree.node('Hinge 1')
+block_cut_tree.node('Hinge 2')
+block_cut_tree.node('Hinge 3')
+block_cut_tree.node('Hinge 4')
+block_cut_tree.node('Hinge 5')
+block_cut_tree.node('Hinge 6')
+block_cut_tree.edge('1', 'Hinge 1')
+block_cut_tree.edge('1', 'Hinge 2')
+block_cut_tree.edge('1', 'Hinge 3')
+block_cut_tree.edge('1', 'Hinge 4')
+block_cut_tree.edge('3', 'Hinge 2')
+block_cut_tree.edge('2', 'Hinge 1')
+block_cut_tree.edge('4', 'Hinge 3')
+block_cut_tree.edge('5', 'Hinge 3')
+block_cut_tree.edge('Hinge 5', 'Hinge 4')
+block_cut_tree.edge('6', 'Hinge 5')
+block_cut_tree.edge('7', 'Hinge 4')
+block_cut_tree.edge('7', 'Hinge 6')
+block_cut_tree.edge('8', 'Hinge 6')
+# nx.algorithms.connectivity.all_pairs_node_connectivity()
+# block_cut_tree.view(filename='block_cut_tree')
 print("m")
 for i in nx.algorithms.connectivity.k_edge_components(Full_graph, 2):
     for j in i:
-        print(j, end = ",")
+        print(j, end=",")
     print()
 print("n")
 print("o")
@@ -168,113 +242,18 @@ for edge in Main_graph.edges(data=True):
     prufer_graph_edges.append((first_index, second_index, edge[2].get('weight')))
 Prufer_graph.add_weighted_edges_from(prufer_graph_edges)
 print(nx.algorithms.to_prufer_sequence(nx.algorithms.tree.minimum_spanning_tree(Prufer_graph)))
-
-
-
-
-# A Python3 program to find size of minimum
-# vertex cover using Binary Search
-
-# Returns true if there is a possible subSet
-# of size 'k' that can be a vertex cover
-def isCover(V, k, E):
-
-    # Set has first 'k' bits high initially
-    Set = (1 << k) - 1
-
-    limit = (1 << V)
-
-    # to mark the edges covered in each
-    # subSet of size 'k'
-    vis = [[None] * maxn for i in range(maxn)]
-
-    while (Set < limit):
-
-        # ReSet visited array for every
-        # subSet of vertices
-        vis = [[0] * maxn for i in range(maxn)]
-
-        # Set counter for number of edges covered
-        # from this subSet of vertices to zero
-        cnt = 0
-
-        # selected vertex cover is the
-        # indices where 'Set' has its bit high
-        j = 1
-        v = 1
-        while(j < limit):
-            if (Set & j):
-
-                # Mark all edges emerging out of
-                # this vertex visited
-                for k in range(1, V + 1):
-                    if (gr[v][k] and not vis[v][k]):
-                        vis[v][k] = 1
-                        vis[k][v] = 1
-                        cnt += 1
-            j = j << 1
-            v += 1
-
-        # If the current subSet covers all the edges
-        if (cnt == E):
-            return True
-
-        # Generate previous combination with k bits high
-        # Set & -Set = (1 << last bit high in Set)
-        c = Set & -Set
-        r = Set + c
-        Set = (((r ^ Set) >> 2) // c) | r
-    return False
-
-# Returns answer to graph stored in gr[][]
-def findMinCover(n, m):
-
-    # Binary search the answer
-    left = 1
-    right = n
-    while (right > left):
-        mid = (left + right) >> 1
-        if (isCover(n, mid, m) == False):
-            left = mid + 1
-        else:
-            right = mid
-
-    # at the end of while loop both left and
-    # right will be equal,/ as when they are
-    # not, the while loop won't exit the
-    # minimum size vertex cover = left = right
-    return left
-
-# Inserts an edge in the graph
-def insertEdge(u, v):
-    gr[u][v] = 1
-    gr[v][u] = 1 # Undirected graph
-
-# Driver code
-maxn = 100
-
-# Global array to store the graph
-# Note: since the array is global,
-# all the elements are 0 initially
-gr = [[None] * maxn for i in range(maxn)]
-
-#
-#     6
-#     /
-#     1 ----- 5 vertex cover = {1, 2}
-#     /|\
-# 3 | \
-# \ | \
-#     2 4
-V = len(Main_graph.nodes)
-E = len(Main_graph.edges)
-for edge in Main_graph.edges:
-    for index, vertex in enumerate(Main_graph.nodes):
-        if vertex == edge[0]:
-            first_index = index
-        if vertex == edge[1]:
-            second_index = index
-    insertEdge(first_index, second_index)
-print("Minimum size of a vertex cover = ",
-                       findMinCover(V, E))
-
+proof1 = gv.Graph()
+proof1.node('1')
+proof1.node('2')
+proof1.node('3')
+proof1.edge('1', '2')
+# proof1.view(filename='proof1')
+proof2 = gv.Graph()
+proof2.node('1')
+proof2.node('2')
+proof2.node('3')
+proof2.edge('1', '2')
+proof2.edge('1', '2')
+proof2.edge('3', '2')
+proof2.edge('1', '3')
+# proof2.view(filename='proof2')
